14ad385acec9dd708104e51327426d65a13e2876
Daniel Milner <danielmmilner@gmail.com>
2019-08-01 19:05:24 +1000

    Changed project to use Vuejs

--- test.js
+++
@@ -1,9 +0,0 @@
-const message = Buffer.from('400|lolololol');
-const client = dgram.createSocket('udp4');
-
-function myFunction() {
-    console.log("Sending")
-    client.send(message, 8888, '192.168.1.108', (err) => {
-        client.close();
-    });
-}
