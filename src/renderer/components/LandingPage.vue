b87cca58c27e1568e374b22917fa3ca6ff098634
Daniel Milner <danielmmilner@gmail.com>
2019-08-05 17:52:43 +1000

    Added sidebar

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,31 +1,25 @@
 <template>
   <div id="wrapper">
-    <PacketSender></PacketSender>
-    <DisplayGenerator></DisplayGenerator>
+    <Sidebar></Sidebar>
+    <div id="content">
+      <PacketSender></PacketSender>
+      <DisplayGenerator></DisplayGenerator>
+    </div>
   </div>
 </template>
 
 <script>
   import DisplayGenerator from './LandingPage/DisplayGenerator'
   import PacketSender from './LandingPage/PacketSender'
+  import Sidebar from './LandingPage/Sidebar'
 
   export default {
     name: 'landing-page',
-    components: { DisplayGenerator, PacketSender }
+    components: { DisplayGenerator, PacketSender, Sidebar }
   }
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
-
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-
-  body { font-family: 'Source Sans Pro', sans-serif; }
-
   #wrapper {
     background:
       radial-gradient(
@@ -33,9 +27,61 @@
         rgba(255, 255, 255, 1) 40%,
         rgba(229, 229, 229, .9) 100%
       );
-    height: 100vh;
-    padding: 60px 80px;
-    width: 100vw;
+    align-items: stretch;
+    display: flex;
+    width: 100%;
   }
 
+  #content {
+    padding: 60px 80px;
+  }
+
+  #sidebar {
+      min-width: 250px;
+      max-width: 250px;
+      min-height: 100vh;
+  }
+
+  #sidebar.active {
+      margin-left: -250px;
+  }
+
+  a, a:hover, a:focus {
+      color: #fff;
+      text-decoration: none;
+      transition: all 0.3s;
+  }
+
+  #sidebar {
+      background: #323538;
+      color: #fff;
+      transition: all 0.3s;
+  }
+
+  #sidebar .sidebar-header {
+      padding: 20px;
+      background: #323538;
+  }
+
+  #sidebar ul.components {
+      padding: 20px 0;
+      border-bottom: 1px solid #545a5f;
+      border-top: 1px solid #545a5f;
+  }
+
+  #sidebar ul li a {
+      padding: 10px;
+      font-size: 1.1em;
+      display: block;
+      text-decoration: none;
+  }
+  #sidebar ul li a:hover {
+      color: #ffffff;
+      background: #43474b;
+  }
+
+  #sidebar ul li.active > a, a[aria-expanded="true"] {
+      color: #fff;
+      background: #43474b;
+  }
 </style>
