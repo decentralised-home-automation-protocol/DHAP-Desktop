44affab32b9067c66e7c219867f78ff09eea7e17
Daniel Milner <danielmmilner@gmail.com>
2019-08-07 16:08:45 +1000

    Added vuedraggable

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="content">
+    <draggable v-model="myArray" group="people" @start="drag=true" @end="drag=false">
+      <div v-for="element in myArray" :key="element.id"><p>{{element}}</p></div>
+    </draggable>
     <PacketSender></PacketSender>
     <DisplayGenerator></DisplayGenerator>
   </div>
@@ -8,10 +11,16 @@
 <script>
   import DisplayGenerator from './LandingPage/DisplayGenerator'
   import PacketSender from './LandingPage/PacketSender'
+  import draggable from 'vuedraggable'
 
   export default {
     name: 'landing-page',
-    components: { DisplayGenerator, PacketSender }
+    data: function () {
+      return {
+        myArray: ['test1', 'test2', 'test3']
+      }
+    },
+    components: { DisplayGenerator, PacketSender, draggable }
   }
 </script>
 
