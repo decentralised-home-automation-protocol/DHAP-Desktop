fc47dca7a43cd2b9bd8f2992c08b3c1d6a5149de
Daniel Milner <danielmmilner@gmail.com>
2019-10-16 20:25:31 +1100

    Improved Packet sender

--- src/renderer/components/sidebar/packet-sender.vue
+++ src/renderer/components/sidebar/packet-sender.vue
@@ -8,12 +8,19 @@
     </div>
     <div class="input-group mb-3">
       <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">Data</span>
+        <select class="form-control" @change="getPacketType($event)">
+          <option v-for="type in packetTypes" v-bind:key="type[1]">{{type[0]}}</option>
+        </select>
       </div>
-      <input class="form-control" type="text" placeholder="300" v-model="packetdata">
-      <div class="input-group-append">
-        <button class="btn btn-outline-info" @click="sendPacket(ipaddress, packetdata)">Send</button>
+    </div>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">{{packetTypes[selectedPacketIndex][1]}}</span>
       </div>
+      <input class="form-control" type="text" :placeholder=packetTypes[selectedPacketIndex][2] v-model="packetData">
+    </div>
+    <div class="input-group mb-3">
+      <button class="btn btn-outline-info btn-block" @click="sendPacket(ipaddress, packetData)">Send</button>
     </div>
   </div>
 </template>
@@ -25,14 +32,30 @@
     name: 'packet-sender',
     data: function () {
       return {
-        packetdata: '200',
-        ipaddress: '192.168.1.108'
+        selectedPacketIndex: 0,
+        packetData: '',
+        ipaddress: '255.255.255.255',
+        packetTypes: [
+          ['Send Credentials', '100|', 'ssid,password'],
+          ['Discovery Request', '300|', 'mac,statusbit,visibilitybit,headerVersion'],
+          ['Header Request', '320|', 'N/A'],
+          ['UI request', '200|', 'N/A'],
+          ['IoT Command', '400|', 'groupID-elementID=elementStatus'],
+          ['Lease Request', '500|', 'duration,interval,response'],
+          ['Leave Lease', '510|', 'N/A'],
+          ['Change Name', '600|', 'name'],
+          ['Change Location', '610|', 'location'],
+          ['Other', '', 'data']
+        ]
       }
     },
     methods: {
       sendPacket (ipaddress, packetdata) {
         console.log('Sending: ' + packetdata)
         server.send(packetdata, 8888, ipaddress)
+      },
+      getPacketType (type) {
+        this.selectedPacketIndex = type.target.selectedIndex
       }
     }
   }
@@ -41,5 +64,9 @@
 <style>
 #PacketSender {
   padding: 10px 10px 0px 10px;
+}
+
+#packetFormat {
+  color: whitesmoke;
 }
 </style>
